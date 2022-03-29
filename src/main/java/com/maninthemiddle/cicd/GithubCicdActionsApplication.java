7d3802d2810151a5803e9c07eae44cbf06158106
Krishnaprasad TV <krishnaprasadtv@Krishnaprasads-MacBook-Pro.local>
2022-03-29 09:10:10 +0530

    rest EP added

--- src/main/java/com/maninthemiddle/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/maninthemiddle/cicd/GithubCicdActionsApplication.java
@@ -2,10 +2,20 @@ package com.maninthemiddle.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
 
+	@GetMapping("/health")
+	public ResponseEntity<String> getHealth(){
+		return new ResponseEntity<>("GOOD",HttpStatus.OK);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
